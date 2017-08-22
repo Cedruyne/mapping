26c038c265b2400a414de2b6fabe73c349907f25
Cédric de Bruyne <cdebruyne@outlook.com>
2017-08-22 10:47:23 +0200

    ajout de la map OSM

--- index.php
+++ index.php
@@ -9,7 +9,6 @@ $geocoder = new \Geocoder\ProviderAggregator();
 $adapter  = new \Http\Adapter\Guzzle6\Client();
 
 // Create a chain of providers.
-// Be sure to include my previously created adapter.
 $chain = new \Geocoder\Provider\Chain\Chain(
         array(
             new \Geocoder\Provider\Nominatim\Nominatim($adapter, "http://nominatim.openstreetmap.org/search"),
@@ -22,39 +21,45 @@ $geocoder->registerProvider($chain);
 // Demo locations
 $locations = array(
     array(
-        'address' => '3324 N California Ave, Chicago, IL, 60618',
-        'title' => 'Hot Dougs',
+        'address' => '82 Avenue Denfert-Rochereau, 75014 Paris',
+        'title' => 'Les Grands Voisins',
     ),
     array(
-        'address' => '11 S White, Frankfort, IL, 60423',
-        'title' => 'Museum',
+        'address' => '46 Rue René Clair, 75018 Paris',
+        'title' => '3W Academy',
     ),
     array(
-        'address' => '1000 Sterling Ave, , Flossmoor, IL, 60422',
-        'title' => 'Library',
-    ),
-    array(
-        'address' => '2053 Ridge Rd, Homewood, IL, 60430',
-        'title' => 'Twisted Q',
+        'address' => '33 Rue Boinod, 75018 Paris',
+        'title' => 'La Piscine',
     )
 );
+
+$mapdata = $marker_group = array();
+
+function markerCreator($lat, $long, $label, $key) {
+    return "var marker{$key} = L.marker([{$lat}, {$long}]).addTo(map);
+    marker{$key}.bindPopup(\"{$label}\");";
+}
 
 foreach ($locations as $key => $value) {
     // Try to geocode.
     try {
         $geocode = $geocoder->geocodeQuery(GeocodeQuery::create($value['address']));
         $coordinates = $geocode->first()->getCoordinates();
-        // var_dump($coordinates);
+        $longitude = $coordinates->getLongitude();
+        $latitude = $coordinates->getLatitude();
+
+        // Create map data array
+        $mapdata[] = markerCreator($latitude, $longitude, $value['title'], $key);
+
+        // Marker grouping array
+        $marker_group[] = "marker{$key}";
 
     } catch (Exception $e) {
         echo $e->getMessage();
     }
+
 }
-
-$resultat = $geocoder->geocode($_SERVER['REMOTE_ADDR']);
-$city = $geocode->first()->getLocality();
-var_dump($city);
-
 ?>
 <!DOCTYPE html>
 <html>
@@ -62,6 +67,14 @@ var_dump($city);
     <title>A simple map with Geocoder PHP and Leaflet.js</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.2.0/dist/leaflet.css" />
+    <script src="https://unpkg.com/leaflet@1.2.0/dist/leaflet.js"></script>
+    <style>
+        #map {
+            width: 800px;
+            height: 400px;
+        }
+    </style>
 </head>
 <body>
 <div class="container">
@@ -71,12 +84,27 @@ var_dump($city);
         </div>
         <div class="row-fluid">
             <div class="col-lg-8">
-
+                <div id="map"></div>
             </div>
         </div>
     </div><!-- /row -->
 </div> <!-- /container -->
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+<script>
+    var map = L.map('map');
+
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>  ',
+        maxZoom: 28
+    }).addTo(map);
+
+//    L.tileLayer('//{s}.tile.cloudmade.com/41339be4c5064686b781a5a00678de62/998/256/{z}/{x}/{y}.png', {maxZoom: 18}).addTo(map);
+
+    <?php print implode('', $mapdata); ?>
+
+    var group = new L.featureGroup([<?php print implode(', ', $marker_group); ?>]);
+    map.fitBounds(group.getBounds());
+</script>
 </body>
 </html>
